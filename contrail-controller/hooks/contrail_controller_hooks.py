acdaec16b799dfd716cda5207b1f83fc696b69cf
OlegBravo <oleg.bravo.ru@gmail.com>
2020-01-27 13:24:48 +0300

    set orchestartion ifo

--- contrail-controller/hooks/contrail_controller_hooks.py
+++ contrail-controller/hooks/contrail_controller_hooks.py
@@ -271,7 +271,6 @@ def update_southbound_relations(rid=None):
         "auth-mode": config.get("auth-mode"),
         "auth-info": config.get("auth_info"),
         "orchestrator-info": config.get("orchestrator_info"),
-        # "orchestrator-info": {"cloud_orchestrator": "openstack", "metadata_shared_secret": "390988bd-e7d9-48aa-948c-7f1db78f3488", "compute_service_ip": "10.0.12.124", "image_service_ip": "10.0.12.109", "network_service_ip": "10.0.12.127"},
         "agents-info": config.get("agents-info"),
         "ssl-enabled": config.get("ssl_enabled") and config.get("config_analytics_ssl_available"),
         # base64 encoded ca-cert
